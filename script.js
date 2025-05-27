7fc9efdb5da6098979dfda3f44f5be1457e99b30
turzo1061 <turzo15-5785@diu.edu.bd>
2025-05-27 14:32:28 +0600

    Name

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function printing(){
-    console.log('Button Clicked')
-}
+function printing() {
+    let a = 5;
+    let b = "Hi";
+    let c = 3.5;
+    let d = "hello";
+    const e = "61_A";
+    
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+    console.log(e);  
+  }
+  
+ 
+  printing();
+  
